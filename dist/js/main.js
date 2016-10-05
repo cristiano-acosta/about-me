0dad5492748717099b7d9c7423587446bad9c895
Cristiano Acosta <cristiano-acosta@hotmail.com>
2016-10-05 17:50:22 -0300

    Deploy Git Page on Docs

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
+// Format dates using plain JS
+// Would be best to use http://momentjs.com/
+
+Handlebars.registerHelper('dateFormat', function (options) {
+	var date = new Date(options * 1000);
+	var d = date.getDate();
+	var m = date.getMonth() + 1; //Months are zero based
+	var y = date.getFullYear();
+
+	return '' + y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+});
+//# sourceMappingURL=handlebars-helpers.js.map
+
+'use strict';
+
 $(function () {
 
   // Get your Behance API Key here:
